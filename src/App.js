39357846064ac42210dea2cec9e8215f51bac859
Matthew Markose <matt.markose@gmail.com>
2025-03-24 16:51:42 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import '@google/model-viewer';
 
-function App() {
+const App = ()=> {
+
+
+
+  const file = "/ammu.glb";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <model-viewer
+            ar
+            camera-controls
+            poster="/poster.jpg"
+            ar-modes="scene-viewer quick-look webxr"
+            src={file}
+            ios-src={"/ammu.usdz"}
+            style={{height: '90vh'}} //added width and max-width.
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </model-viewer>
+      </div>
   );
+
+  /*
+    let file =
+        'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf';
+    const browser_fallback_url = 'https://developers.google.com/ar';
+    const packageApp = 'com.google.android.googlequicksearchbox';
+
+    const intentUrl = `intent://arvr.google.com/scene-viewer/1.0?file=${file}#Intent;scheme=https;package=${packageApp};action=android.intent.action.VIEW;S.browser_fallback_url=${browser_fallback_url};end;`;
+
+    return <div>
+      <a href={intentUrl}>View in AR</a>
+    </div>;
+
+  */
 }
 
 export default App;
